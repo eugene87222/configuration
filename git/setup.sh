806c6a5c0a7ef4b99ffc6c550a63cd275543f682
Eugene Yang <eugene87222@gmail.com>
2022-10-09 00:13:50 +0800

    add git setup script, update starship layout

---
+++ git/setup.sh
@@ -0,0 +1,5 @@
+git config --global user.email "eugene87222@gmail.com"
+git config --global user.name "Eugene Yang"
+git config --global color.ui true
+git config --global core.editor vim
+git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
